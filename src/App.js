3596ca526a040b28d223c7383ed23496fadfb01c
Rana Bhattacharyya <ranab33@gmail.com>
2019-08-18 16:00:21 -0400

    lotto ball component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Ball from "./Ball";
 
 function App() {
   return (<div className="App" >
-
+    <Ball num={17} />
+    <Ball num={7} />
+    <Ball num={11} />
+    <Ball num={77} />
   </div>
   );
 }
